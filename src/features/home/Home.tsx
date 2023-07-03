60cadb0186833b9a30b7ddbc23b68e4205e233c8
Diego Corradi <diego.corradi@archivagroup.it>
2023-07-03 16:53:24 +0200

    home refactoring

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,40 +1,51 @@
+import SearchIcon from '@mui/icons-material/Search';
+import { Box, TextField } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 import { CitiesProps } from '../../model/CitiesProps';
 import { fetchCities } from '../../services/fetchCities';
-import Error from '../../shared/components/Error';
-import Spinner from '../../shared/components/Spinner';
-import { SearchBox } from './SearchBox';
+import { HOCData } from '../../shared/HOCData';
+import TableData from '../../shared/TableData';
 import { CITIES_APIKEY } from '../../shared/citiesApiKey';
+import Sidebar from '../../shared/components/Sidebar';
 
 const Home = () => {
-  const { isLoading, error, data } = useQuery<
-    boolean,
-    AxiosError<any, any>,
-    CitiesProps
-  >(['cities'], () => fetchCities(CITIES_APIKEY, 'Sommacampagna'));
+  const infoUseQuery = useQuery<boolean, AxiosError<any, any>, CitiesProps>(
+    ['cities'],
+    () => fetchCities(CITIES_APIKEY, 'Sommacampagna')
+  );
 
   return (
-    <div className="container mx-auto px-12 min-h-screen flex flex-col">
-      <div className="flex flex-col justify-around items-center">
-        <h1 className="text-4xl font-bold text-primary md:text-7xl z-10">
-          PIZZA FINDER
-        </h1>
-        <div className="flex items-center justify-center">
-          <SearchBox />
+    <div className="container mx-auto px-12 min-h-screen flex flex-col justify-around">
+      <div className="flex flex-col items-òl justify-center">
+        <div className="flex">
+          <h1 className="text-4xl font-bold text-primary md:text-7xl z-10">
+            PIZZA FINDER
+          </h1>
+          <Sidebar />
         </div>
-        <div>
-          {!error ? (
-            <div className="flex flex-col">
-              {data?.results.map((result) => (
-                <li key={result.formatted}>{result.formatted}</li>
-              ))}
-            </div>
-          ) : (
-            <Error message={error.message} />
-          )}
-          {isLoading && <Spinner />}
-        </div>
+        <Box
+          component="form"
+          sx={{
+            '& > :not(style)': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="outlined-basic"
+            label="Search city"
+            variant="outlined"
+          />
+          <button>
+            <SearchIcon />
+          </button>
+        </Box>
+      </div>
+      <div>
+        <HOCData infoUseQuery={infoUseQuery}>
+          <TableData data={infoUseQuery?.data?.results} />
+        </HOCData>
       </div>
     </div>
   );
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,11 @@
 /// <reference types="vite/client" />
+
+import { Result } from './model/CitiesProps';
+
 interface ImportMetaEnv {
   readonly VITE_API_KEY: string;
+}
+
+export interface TableProps {
+  data?: Result[];
 }
