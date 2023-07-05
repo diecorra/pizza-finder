cf8b6950cb4c405aa563060d7bfb7318a9092f35
Diego Corradi <diego.corradi@archivagroup.it>
2023-07-05 15:08:15 +0200

    feat: home features complete, start implementing city page

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -2,7 +2,7 @@ import SearchIcon from '@mui/icons-material/Search';
 import { useQuery } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 import { useState } from 'react';
-import { CitiesProps } from '../../model/CitiesProps';
+import { Result } from '../../model/CitiesProps';
 import { fetchCities } from '../../services/fetchCities';
 import { HOCData } from '../../shared/HOCData';
 import TableData from '../../shared/TableData';
@@ -11,7 +11,7 @@ import { CITIES_APIKEY } from '../../shared/citiesApiKey';
 const Home = () => {
   const [input, setInput] = useState('');
 
-  const infoUseQuery = useQuery<boolean, AxiosError<any, any>, CitiesProps>(
+  const infoUseQuery = useQuery<boolean, AxiosError<any, any>, Result[]>(
     ['city'],
     () => fetchCities(CITIES_APIKEY, input),
     { enabled: false }
@@ -27,7 +27,7 @@ const Home = () => {
   };
 
   return (
-    <div className="flex flex-col justify-around gap-28">
+    <div className="flex flex-col justify-around gap-10">
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-4xl font-bold text-primary md:text-7xl z-10">
           PIZZA FINDER
@@ -39,16 +39,21 @@ const Home = () => {
             pattern=".{3,}"
             type="text"
             name="city"
-            placeholder="Search city..."
+            placeholder="Search city, town or ..."
             value={input}
             onChange={(e) => handleInputValue(e.target.value)}
           />
-          <button className="mx-3" type="submit">
+          <button className="mx-2" type="submit">
             <SearchIcon sx={{ color: '#130F49', cursor: 'pointer' }} />
           </button>
+          <p className="w-7/12 text-black mr-2">
+            {infoUseQuery?.data && !infoUseQuery?.error
+              ? 'Results: ' + infoUseQuery?.data?.length
+              : 'Results: ' + 0}
+          </p>
         </form>
         <HOCData infoUseQuery={infoUseQuery}>
-          <TableData data={infoUseQuery?.data?.results}></TableData>
+          <TableData data={infoUseQuery?.data}></TableData>
         </HOCData>
       </div>
     </div>
--- src/services/fetchCities.ts
+++ src/services/fetchCities.ts
@@ -5,5 +5,11 @@ export const fetchCities = async (apiKey: string, city: string) => {
     ` https://api.opencagedata.com/geocode/v1/json?key=${apiKey}&q=${city}`
   );
 
-  return result.data;
+  const resultsOnlyCities = result.data.results.filter(
+    (result) =>
+      result.components._type === 'city' ||
+      result.components._type === 'village'
+  );
+
+  return resultsOnlyCities;
 };
