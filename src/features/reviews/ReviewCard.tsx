2004b88d079a5f0505f7c79d0fd4bbe853986bf9
diecorra <diego.corradi95@gmail.com>
2023-09-24 22:05:45 +0200

    fix review height cards

--- src/features/reviews/ReviewCard.tsx
+++ src/features/reviews/ReviewCard.tsx
@@ -9,13 +9,15 @@ import {
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import { Review } from 'model/review';
+import { useState } from 'react';
 import { dateFormat } from 'utils/dateFormat';
 
 export default function ReviewCard({ dataReview }: { dataReview: Review }) {
   const { user, title, description, img, pizzeria, rate, created } = dataReview;
+  const [isImgClicked, setIsImgClicked] = useState(false);
 
   return (
-    <Card className="max-w-[20rem] min-h-[20rem] flex flex-col justify-between">
+    <Card className="min-h-[15rem] max-h-96 flex flex-col justify-between">
       <div>
         <CardHeader
           avatar={
@@ -27,16 +29,20 @@ export default function ReviewCard({ dataReview }: { dataReview: Review }) {
           }
           title={pizzeria}
           subheader={created ? dateFormat(created) : dateFormat(new Date())}
-          titleTypographyProps={{ fontSize: '0.85rem', fontWeight: 'bold' }}
+          titleTypographyProps={{
+            fontSize: '0.85rem',
+            fontWeight: 'bold',
+            overflowWrap: 'break-word',
+          }}
           subheaderTypographyProps={{ fontSize: '0.7rem' }}
         />
         <CardContent>
-          <div className="flex justify-between">
-            <h3>{title}</h3>
+          <div className="flex flex-col">
             <div className="flex items-center">
               <StarIcon sx={{ color: 'orange' }} />
               <p>{rate}</p>
             </div>
+            <h3>{title}</h3>
           </div>
           <Typography
             variant="body2"
@@ -49,10 +55,15 @@ export default function ReviewCard({ dataReview }: { dataReview: Review }) {
       </div>
       {img ? (
         <CardMedia
-          className="h-40 w-full object-cover"
+          className={
+            !isImgClicked
+              ? 'h-24 w-full object-cover hover:cursor-pointer'
+              : 'hidden absolute z-[100] top-0 left-0 max-w-sm max-w-sm bg-contain bg-no-repeat bg-center bg-black hover:cursor-pointer'
+          }
           component="img"
           image={img}
           alt={pizzeria}
+          onClick={() => setIsImgClicked(!isImgClicked)}
         />
       ) : null}
     </Card>
